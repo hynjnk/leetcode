81f345e8bb8a1198a6959884de77c33037eabf08
H. Kim <38238305+hachijo31@users.noreply.github.com>
2018-06-20 12:37:21 +0900

    Make change the solution of two_sum. Using structure dict, and one-pass hash table approch. https://leetcode.com/problems/two-sum/solution/ 	modified:   two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python3
 
+# twoSum_naive  : brute force
+# twoSum_dict   : using solution 3 - One-pass Hash Table https://leetcode.com/problems/two-sum/solution/
+
 
 class Solution:
     def twoSum(self, nums, target):
-        return self.twoSum_naive(nums, target)
+        return self.twoSum_dict(nums, target)
 
     def twoSum_naive(self, nums, target):
         """
@@ -17,11 +20,13 @@ class Solution:
                     return [i, j]
 
     def twoSum_dict(self, nums, target):
-        numsDict = {v: i for i, v in enumerate(nums)}
-        for v in nums:
+        numsDict = {}
+        for i, v in enumerate(nums):
             complement = target - v
-            if (complement != v) and (complement in numsDict):
-                return [numsDict[v], numsDict[complement]]
+            if complement in numsDict:
+                return [i, numsDict[complement]]
+            else:
+                numsDict[v] = i
 
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
